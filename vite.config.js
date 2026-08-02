f191b69ea94266f45f667dc4820db2f0ed6e52ec
Mai <morn43n4mai@gmail.com>
2026-08-02 16:45:46 +0900

    さっしーFB

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,7 @@ import path from "path";
 export default defineConfig({
    base: "./",
   resolve: {
-    alias: {
-      "@sass": path.resolve(__dirname, "src/sass"),
-      "@images": path.resolve(__dirname, "src/images"),
-    },
+   
   },
     build: {
     rollupOptions: {
